de9eaa1bfe0677ffda60234e954f9ca040786505
Ubirajara Neves <ubirajara.neves@usp.br>
2022-06-22 07:48:28 -0300

    Menu básico

---
+++ ssm.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	sv "github.com/biraneves/screen-visual"
+)
+
+const VERSION = "0.1.0"
+
+func main() {
+
+	var option  int
+
+	show_menu()
+	fmt.Scanf("%d", &option)
+
+}
+
+func show_menu() {
+
+	title := fmt.Sprint("Site Status Monitor - v. ", VERSION)
+	fmt.Println("")
+	fmt.Println(title)
+	sv.Line("=", len(title))
+	fmt.Println("[1] Iniciar monitoramento")
+	fmt.Println("[2] Exibir logs")
+	fmt.Println("[0] Sair")
+	sv.Line("-", len(title))
+	fmt.Print("Sua opção: ")
+
+}
